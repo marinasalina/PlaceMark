64ff96186c73c1177c20c592ed2748a0ec374943
Marina19761107 <marinasal1107@gmail.com>
2026-03-12 23:27:00 +0000

    implement play;ist-api route handler (create)

--- src/api/placemark-api.js
+++ src/api/placemark-api.js
@@ -14,7 +14,19 @@ export const placemarkApi = {
 
   create: {
     auth: false,
-    handler: async function (request, h) {},
+    handler: async function (request, h) {
+      try {
+        const placemarklist = request.payload;
+        const newPlacemarklist =
+          await db.placemarklistStore.addPlacemarklist(placemarklist);
+        if (newPlacemarklist) {
+          return h.response(newPlacemarklist).code(201);
+        }
+        return Boom.badImplementation("error creating playlist");
+      } catch (err) {
+        return Boom.serverUnavailable("Database Error");
+      }
+    },
   },
 
   deleteOne: {
