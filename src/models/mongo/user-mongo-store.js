5fd34574e1a7d11f491333626018c9c59593da8a
Marina19761107 <marinasal1107@gmail.com>
2026-03-12 20:42:48 +0000

    user api tests.js

--- src/models/mongo/user-mongo-store.js
+++ src/models/mongo/user-mongo-store.js
@@ -35,7 +35,7 @@ export const userMongoStore = {
     }
   },
 
-  async deleteAll() {
+  async deleteAllUsers() {
     await User.deleteMany({});
   },
 };
--- test/api/user-api-test.js
+++ test/api/user-api-test.js
@@ -1,15 +1,61 @@
 import { assert } from "chai";
-import { playtimeService } from "./playtime-service.js";
+import { placemarkService } from "./placemark-service.js";
 import { assertSubset } from "../test-utils.js";
-import { maggie } from "../fixtures.js";
+import { maggie, testUsers } from "../fixtures.js";
 
 suite("User API tests", () => {
-  setup(async () => {});
+  setup(async () => {
+    await placemarkService.deleteAllUsers();
+    for (let i = 0; i < testUsers.length; i += 1) {
+      // eslint disable next line no-await-in-loop
+      testUsers[i] = await placemarkService.createUser(testUsers[i]);
+    }
+  });
   teardown(async () => {});
 
   test("create a user", async () => {
-    const newUser = await playtimeService.createUser(maggie);
+    const newUser = await placemarkService.createUser(maggie);
     assertSubset(maggie, newUser);
     assert.isDefined(newUser._id);
+  });
+  test("delete all users", async () => {
+    let returnedUsers = await placemarkService.getAllUsers();
+    assert.equal(returnedUsers.length, 3);
+    await placemarkService.deleteAllUsers();
+    returnedUsers = await placemarkService.getAllUsers();
+    assert.equal(returnedUsers.length, 0);
+  });
+
+  test("get a user - success", async () => {
+    const returnedUser = await placemarkService.getUser(testUsers[0]._id);
+    assert.deepEqual(testUsers[0], returnedUser);
+  });
+  test("get a user - fail", async () => {
+    try {
+      const returnedUser = await placemarkService.getUser("1234");
+      assert.fail("Should not return a response");
+    } catch (error) {
+      assert(error.response.data.message === "No User with this id");
+    }
+  });
+  test("get a user - bad id", async () => {
+    try {
+      const returnedUser = await placemarkService.getUser("1234");
+      assert.fail("Should not return a response");
+    } catch (error) {
+      assert(error.response.data.message === "No User with this id");
+      assert.equal(error.response.data.statusCode, 404);
+    }
+  });
+
+  test("get a user - deleted user", async () => {
+    await placemarkService.deleteAllUsers();
+    try {
+      const returnedUser = await placemarkService.getUser(testUsers[0]._id);
+      assert.fail("Should not return a response");
+    } catch (error) {
+      assert(error.response.data.message === "No User with this id");
+      assert.equal(error.response.data.statusCode, 404);
+    }
   });
 });
