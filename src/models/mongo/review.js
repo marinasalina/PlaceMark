c3bef38ff696cf085ef561198a40ac2eb9c13d15
Marina19761107 <marinasal1107@gmail.com>
2026-04-11 17:54:16 +0100

    add new file - review.js

---
+++ src/models/mongo/review.js
@@ -0,0 +1,32 @@
+import Mongoose from "mongoose";
+
+const { Schema } = Mongoose;
+
+const reviewSchema = new Schema({
+  user: {
+    type: Schema.Types.ObjectId,
+    ref: "User",
+    required: true,
+  },
+  placemark: {
+    type: Schema.Types.ObjectId,
+    ref: "Placemark",
+    required: true,
+  },
+  text: {
+    type: String,
+    required: true,
+  },
+  rating: {
+    type: Number,
+    min: 1,
+    max: 5,
+    default: 5,
+  },
+  date: {
+    type: Date,
+    default: Date.now,
+  },
+});
+
+export const Review = Mongoose.model("Review", reviewSchema);
